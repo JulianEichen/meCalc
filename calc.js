487029df9926e3c2275e0c2cec19ec50bf6965cc
Julian <juleichen@aol.com>
2022-06-30 13:51:51 +0200

    finished calc logic, finished special button logic, added show()

--- calc.js
+++ calc.js
@@ -1,7 +1,8 @@
 // containers for 
 const calculator = {
     disp: document.querySelector('#display'),
-    prev_operator: 'equals',
+    locked_operator: 'equals',
+    prev_click: 'none',
     max_in_length: 9,
     input: '0',
     memory: '0',
@@ -11,89 +12,156 @@ const calculator = {
 createUI();
 updateDisplay('0');
 
-function createUI(){
+function createUI() {
     // special buttons
     const ac_button = document.querySelector('#ac_button');
     ac_button.addEventListener('click', resetClac);
 
+    const sgn_button = document.querySelector('#sgn_button');
+    sgn_button.addEventListener('click', clickSgn);
+
+    const prc_button = document.querySelector('#prc_button');
+    prc_button.addEventListener('click', clickPrc);
+
     // number buttons
     const num_buttons = document.querySelectorAll('.number_button');
-    num_buttons.forEach((btt) =>{
+    num_buttons.forEach((btt) => {
         btt.addEventListener('click', clickNumber);
     });
 
     // operator buttosn
-    const opr_buttons = document.querySelectorAll('.operator_buttons');
-    opr_buttons.forEach((btt) =>{
+    const opr_buttons = document.querySelectorAll('.operator_button');
+    opr_buttons.forEach((btt) => {
         btt.addEventListener('click', clickOperator);
     })
 }
 
 // button functions
-function resetClac(){
-    calculator.operator = 'equals';
+function resetClac() {
+    calculator.locked_operator = 'equals';
+    calculator.prev_click = 'none';
     calculator.input = '0';
     calculator.memory = '0';
-    calculator.disp.textContent = '0';
+    show('0');
 }
 
-function clickNumber(event){
-    val  = event.target.value;
+function clickSgn() {
+    var txtCont = calculator.disp.textContent;
+    var x = parseFloat(txtCont);
+    if (txtCont.length >= calculator.max_in_length & x > 0) {
+        return;
+    } else {
+        x *= -1;
+        show(x);
+    }
+}
+
+function clickPrc() {
+    console.log('prc a')
+    var txtCont = calculator.disp.textContent;
+    var x = parseFloat(txtCont);
+
+    // case 1 (txt.length >= max_in_length): 123456789 -> 1234567.89 => txt.length = 10
+    var case_1 = (txtCont.length >= calculator.max_in_length);
+
+    // case 2a (x<1000 &txt.length >= max_in_length-1): 765.4321 -> 7.654321 => txt.length = 8
+    // case 2b (x<100 &txt.length >= max_in_length-1): 76.54321 -> 0.7654321 => txt.length = 9
+    // case 2b (x<1 & txt.length >= max_in_length-1): 0.654321 -> 0.00654321 => txt.length = 10
+    var case_2b = (x<1 & txtCont.length >= calculator.max_in_length-1);
+
+    // case 3a (x<0 & txt.length >= max_in_length-1): -7654321 -> -76543.21 => txt.length = 9
+    // case 3b (x<0 & txt.length >= max_in_length-1): -65.4321 -> -0.654321 => txt.length = 9
+    // case 3c (x<0 & |x|<100 & txt.length >= max_in_length-1): -65.4321 -> -0.654321 => txt.length = 9
+    // case 3c (x<0 & |x|<10 & txt.length >= max_in_length-1): -6.4321 -> -00.654321 => txt.length = 10
+    var case_3c = (x<0 & Math.abs(x)<10 & txtCont.length >= calculator.max_in_length-1);
+
+    // case 4 (x<0 & txt.length >= max_in_length-2): -654321 -> -6543.21 => txt.length = 8
+    // case 4b (x<0 & |x|<100 & txt.length >= max_in_length-2): -54.321 -> -0.54321 => txt.length = 8
+    // case 4c (x<0 & |x|<10 & txt.length >= max_in_length-2): -6.4321 -> -0.0654321 => txt.length = 9
+
+    if (case_1 || case_2b || case_3c){ 
+        return;                                             
+    }else {
+        x *= 0.01;
+        show(x);
+    }
+
+}
+
+function clickNumber(event) {
+    val = event.target.value;
+    if (calculator.prev_click == 'operator') {
+        calculator.memory = calculator.disp.textContent;
+        show('0');
+    }
     updateDisplay(val);
+    calculator.prev_click = 'number';
 }
 
-function updateDisplay(input){
+function updateDisplay(input) {
     current_cont = calculator.disp.textContent;
 
     // check max length
-    if (current_cont.length>=calculator.max_in_length){
+    if (current_cont.length >= calculator.max_in_length) {
         return;
     }
 
     var new_cont = current_cont;
-    if (input != "."){ 
-        if (current_cont == "0"){ // Display shows a single 0
+    if (input != ".") {
+        if (current_cont == "0") { // Display shows a single 0
             new_cont = input;
-        }else{
+        } else {
             new_cont = current_cont.concat(input);
         }
-    }else{ // input is "."
-        if (current_cont == "0"){ // Display shows a single 0
+    } else { // input is "."
+        if (current_cont == "0") { // Display shows a single 0
             new_cont = "0.";
-        }else if(current_cont.includes(".")){ // user wants to add another "."
+        } else if (current_cont.includes(".")) { // user wants to add another "."
             new_cont = current_cont;
-        }else{
+        } else {
             new_cont = current_cont.concat(input);
         }
     }
-    calculator.disp.textContent = new_cont;
+    show(new_cont);
 }
 
-function clickOperator(event){
-    oprtr  = event.target.value;
-    runCalc(oprtr);
+function show(content){
+    var text = content.toString();
+    if (text.length >= calculator.max_in_length || text == 'NaN'){
+        text = 'NaN'
+    }
+    calculator.disp.textContent = text;
+}
+
+function clickOperator(event) {
+    oprtr = event.target.value;
+
+    var result = runCalc(calculator.locked_operator).toString();
+  
+    show(result); // display result
+    calculator.locked_operator = oprtr;
+    calculator.prev_click = 'operator';
+
 }
 
 // calculation
-function runCalc(oprtr){
-    const x = calculator.memory;
+function runCalc(oprtr) {
+    const x = parseFloat(calculator.memory);
     const y = parseFloat(calculator.disp.textContent);
     var z = 0;
 
-    if (calculator.prev_operator=='add'){
-        z = x+y;
-    }else if (calculator.prev_operator=='substract'){
-        z = x-y;
-    }else if (calculator.prev_operator=='multiply'){
-        z = x*y;
-    }else if (calculator.prev_operator=='divide'){
-        z = x/y;
-    }else if (calculator.prev_operator=='equals'){
+    if (calculator.locked_operator == 'add') {
+        z = x + y;
+    } else if (calculator.locked_operator == 'substract') {
+        z = x - y;
+    } else if (calculator.locked_operator == 'multiply') {
+        z = x * y;
+    } else if (calculator.locked_operator == 'divide') {
+        z = x / y;
+    } else if (calculator.locked_operator == 'equals') {
         z = y;
     }
-    calculator.memory = z;
-    calculator.disp.textContent = 0;
-    calculator.prev_operator = oprtr;
+    return z;
 }
 
 
